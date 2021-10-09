1e9de3892750b27feabd0fa8f25ffff699777f12
Sajedur Rahman Fiad <neural.gen.official@gmail.com>
2021-10-09 20:09:14 +0600

    Update Linear_Regression.py

--- LinearRegression/Linear_Regression.py
+++ LinearRegression/Linear_Regression.py
@@ -5,24 +5,32 @@ class xlsx:
         self.file = pd.read_excel(filename, sheet_name='Sheet1')    # Read the file data
 
     def data(self):
-	# Rearrange the datas in a list and return
+    # Rearrange the datas in a list and return
         self.title = list(self.file)
         datas = []
         for i in self.title:
             datas.append(list(self.file[i]))
-            
+
         return datas
 
-        
+    def titles(self):
+        self.title = list(self.file)
+        return (self.title)
 
-class LinearRegression: 
+
+
+class LinearRegression:
     X, Y, slopes, c = [], [], [], 0   #  Initialize the variables
+    means, squares, XYs, meansOfXY, meansOfSquared, squaresOfMeans, yValues = [], [], [], [], [], [], []  # Temporary lists for calculation
+    sumOfX, sumOfY, sumOfSquares, sumOfProducts = [], [], [], []
+
     data_fitted, predicted = False, False
 
 
     def fit(self, data):   #  Fit the data
         self.X = data[0]   #  Initialize X values
-        self.Y = data[1]   #  Initialize Y values
+        self.Y = data[-1]   #  Initialize Y values
+        self.data = data
         self.data_fitted = True
 
 
@@ -40,7 +48,7 @@ class LinearRegression:
             squared.append(y*y)
         return squared
 
-    
+
     def mean(self, x):   #  Find mean of listed numbers
         # Mean of x, y, z is (x+y+z)/3
         mean_val = 0
@@ -48,54 +56,104 @@ class LinearRegression:
             mean_val += i
         return mean_val/len(x)
 
-    
+
     def slope(self, x, y):   #  Find the slope
         #  m = [{mean(x)*mean(y) - mean(x*y)} / {mean(x^2) - mean(x^2)}]
         lob = (self.mean(x)*self.mean(y)) - (self.mean(self.mul(x, y)))
         hor = (self.mean(x)*self.mean(x)) - self.mean(self.square(x))
         return lob/hor
 
-    
-    def intercept(self, *args):   #  Find the Y-Intercept
+
+    def intercept(self):   #  Find the Y-Intercept
         #  c = mean(y) - m*mean(x)
-        args = list(args)
-        y = args[-1]   #  Initialize y value
-        intercept = self.mean(y)
-        del args[-1]
-        for i in range(len(args)):
-            intercept -= self.slope(args[i], y)*self.mean(args[i])
-        return intercept
+        self.c = self.means[-1]
+        for i in range(len(self.slopes)):
+            self.c -= self.slopes[i] * self.means[i]
+
+        print("Y-Intercept (α0) =", self.c)
 
 
-    def slope_calc(self):   #  Calculate the needed slopes
-        for i in range(len(self.X)):
-            self.slopes.append(self.slope(self.X[i], self.Y))
+    def initialize_data(self):
+        self.yValues = self.data[-1]
 
-        
-    def predict(self, x):    #  Predict the Y values depending on X
+        for i in self.data[:-1]:
+            self.sumOfX.append(sum(i))
+        self.sumOfY = sum(self.yValues)
+
+        for i in self.data:
+            self.means.append(self.mean(i))
+
+        for i in self.data[:-1]:
+            self.squares.append(self.square(i))
+
+        for i in self.squares:
+            self.meansOfSquared.append(self.mean(i))
+
+        for i in self.data[:-1]:
+            self.XYs.append(self.mul(i, self.data[-1]))
+
+        for i in self.XYs:
+            self.meansOfXY.append(self.mean(i))
+
+        for i in self.squares:
+            self.sumOfSquares.append(sum(i))
+
+        for i in self.means[:-1]:
+            self.squaresOfMeans.append(i*i)
+
+        print("Means of [X1, X2, Y] =", self.means)
+        print("Squared [X1, X2, Y] =", self.squares)
+        print("Multiplications of xy [X1*Y, X2*Y] =", self.XYs)
+        print("Means of the multiplications [mean of X1*Y, mean of X2*Y] =", self.meansOfXY)
+
+    def slope_calc(self):
+        lob, hor = [], []
+
+        for i in range(len(self.data)-1):
+            lob.append((self.means[i]*self.means[-1]) - self.meansOfXY[i])
+
+        for i in range(len(self.data)-1):
+            hor.append(self.squaresOfMeans[i] - self.meansOfSquared[i])
+
+        for i in range(len(lob)):
+            self.slopes.append(lob[i] / hor[i])
+
+        print("Slopes [α1, α2] =", self.slopes)
+
+
+    def predict(self, features):    #  Predict the Y values depending on X
                 if self.data_fitted:
                         pass
                 else:
                         raise Exception("DataNotFittedError : Call fit method before calling predict method in order to fit the training data to the model")
-                
-                m = self.slope(self.X, self.Y)   #  Calculate the slopes
-                c = self.intercept(self.X, self.Y)   #  Calculate the intercepts
-                self.predicted = True
-                return (m*x)+c
 
-        
+                self.initialize_data()
+                self.slope_calc()
+                self.intercept()
+                slopes = self.slopes
+
+                result = 0
+
+                for i in range(len(features)):
+                    result += (self.slopes[i]*features[i])
+
+                self.predicted = True
+
+                return result+self.c
+
+
     def r_squared(self):    # Calculate the r-squared value
                 if self.predicted:    # If the predict methos isn't called, raise an error
                         pass
                 else:
                         raise Exception("NotPredictedError : Call predict method first to calculate the r-squared value.")
-                
+
                 original_y = self.Y
                 predicted_y = list(map(self.predict, self.X))    # Predict all Y values by our model
                 lob, hor = 0, 0
 
                 # Apply the formula
-                for i in range(len(original_y))
+                for i in range(len(original_y)):
                         lob += (predicted_y[i]-self.mean(original_y))**2
                 for i in range(len(original_y)):
                         hor += (original_y[i]-self.mean(original_y))**2
@@ -108,20 +166,21 @@ class LinearRegression:
             pass
         else:
             raise Exception("DataNotFittedError : Call fit method before calling plot method in order to plot the model")
-        
+
         import matplotlib.pyplot as plt    # Import matplotlib for plotting and scattering
-        
+
         plt.scatter(self.X, self.Y)    # Scatter the data points first
         predicted_y = list(map(self.predict, self.X))    # Predict all Y values by our model
         plt.plot(self.X, predicted_y)    # Plot the best fit line
         plt.show()    # Show the plot
 
-        
+
 
 if __name__ == '__main__':
     model = LinearRegression()  # Create the model
 
-    data = xlsx('data.xlsx')  # Read the data from data.xlsx
-    model.fit(data.data())  # Fit the data to our model
-    print(model.predict(10))  # Print the price of 10 inch
-    model.plot()  # Plot the model into the graph
+    model.fit([[60, 62, 67, 70, 71, 72, 75, 78], [22, 25, 24, 20, 15, 14, 14, 11], [140, 155, 159, 179, 192, 200, 212, 215]])  # Fit the data to our model
+    #model.fit([[6, 8, 12, 14, 18], [350, 775, 1150, 1395, 1675]])
+    
+    print("\nX1 = 60, X2 = 22, Y =", model.predict([60, 22]), "(Predicted)")
+    print("X1 = 60, X2 = 22, Y =", model.data[-1][0], "(Original)")
